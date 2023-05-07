7ed70cc74427a1b9412e8241d5df1c5706838028
dinhhanpw <dinhhan26031999@gmail.com>
2023-05-07 11:11:53 +0700

    apply Graylog

--- graylog-demo/Program.cs
+++ graylog-demo/Program.cs
@@ -1,6 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection;
+using Gelf.Extensions.Logging;
+using Serilog;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+ //Add services to the container.
+var loggerConfig = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(loggerConfig);
+//builder.Logging.AddGelf(options =>
+//{
+//    options.LogSource = builder.Environment.ApplicationName;
+//    options.Host = "127.0.0.1";
+//    options.AdditionalFields["machine_name"] = Environment.MachineName;
+//    options.AdditionalFields["app_version"] = Assembly.GetEntryAssembly()
+//                    ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
